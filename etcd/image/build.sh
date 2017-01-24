57cf2c4caa2f95909e7d1730c8b0382c45543d91
gaoguangjun <gaogaungjun1@weiabngong.com>
2017-01-24 15:31:53 +0800

    add apiserver compose

--- etcd/image/build.sh
+++ etcd/image/build.sh
@@ -5,4 +5,4 @@ path=$(cd "$(dirname "$0")" &&  pwd)
 cd $path
 #
 docker build --rm=true --no-cache=true -t docker.weibangong.me/etcd-cluster:v3.1.0 .
-docker push docker.weibangong.me/etcd-cluster:v3.1.0
+#docker push docker.weibangong.me/etcd-cluster:v3.1.0
