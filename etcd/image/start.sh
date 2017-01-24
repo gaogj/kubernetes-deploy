200f04ab0408dd3719d7c27a899a5f1b3512737c
gaoguangjun <gaogaungjun1@weiabngong.com>
2017-01-24 13:17:16 +0800

    修复bind ip 错误

--- etcd/image/start.sh
+++ etcd/image/start.sh
@@ -18,7 +18,7 @@ mkdir -p /home/data && mkdir -p /home/logs
 #
 nohup /usr/local/bin/etcd \
     --data-dir=/home/data --name ${THIS_NAME} \
-    --initial-advertise-peer-urls http://${THIS_IP}:2380 --listen-peer-urls http://${THIS_IP}:2380 \
-    --advertise-client-urls http://${THIS_IP}:2379 --listen-client-urls http://${THIS_IP}:2379 \
+    --initial-advertise-peer-urls http://${THIS_IP}:2380 \
+    --advertise-client-urls http://${THIS_IP}:2379 \
     --initial-cluster ${CLUSTER} \
     --initial-cluster-state ${CLUSTER_STATE} --initial-cluster-token ${CLUSTER_NAME} >> /home/logs/etcd.log &
